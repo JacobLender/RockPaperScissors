6a8f4175e55a732c8d3f72db0d6b0c146226d67d
Jacob Lender <69373851+JacobLender@users.noreply.github.com>
2020-08-10 13:21:03 -0600

    Add files via upload

---
+++ Gmae.py
@@ -0,0 +1,99 @@
+import random
+
+def Menu():#This is a
+    PlayerChoice = 0
+    print("Menu\n1. Human Vs Human\n2. Human vs Computer\n3. Exit")
+
+    while PlayerChoice != 1 and PlayerChoice != 2 and PlayerChoice != 3:
+        PlayerChoice = int(input("Please choose a game mode: (1,2,3)"))
+
+    if PlayerChoice == 1:
+        PvPGame()
+    elif PlayerChoice == 2:
+        PvCGame()
+    else:
+        return
+
+def PlayAgain(GameMode):
+    PlayAgainAnswer = input("Would you like to play again? (Y/N)").capitalize()
+
+    if(PlayAgainAnswer == "Y" and GameMode == "PvP"):
+        PvPGame()
+    elif(PlayAgainAnswer == "Y" and GameMode == "PvC"):
+        PvCGame()
+    else:
+
+        print("Alright Whatever. Taking you back to menu.\n")
+        print(Menu())
+
+
+def WinnerDecider(PlayerOne, PlayerTwo):
+    if PlayerOne == "Rock":
+        if PlayerTwo == "Rock":
+            Winner = "Draw"
+        elif PlayerTwo == "Paper":
+            Winner = "PlayerTwo"
+        else:
+            Winner = "PlayerOne"
+    elif (PlayerOne == "Paper"):
+        if PlayerTwo == "Paper":
+            Winner = "Draw"
+        elif PlayerTwo == "Scissor":
+            Winner = "PlayerTwo"
+        else:
+            Winner = "PlayerOne"
+    else:
+        if PlayerTwo == "Scissor":
+            Winner = "Draw"
+        elif PlayerTwo == "Rock":
+            Winner = "PlayerTwo"
+        else:
+            Winner = "PlayerOne"
+
+    return Winner
+
+def PvPGame():
+    GameMode = "PvP"
+    RPS = ["Rock", "Paper", "Scissor"]
+    PlayerOne = 999
+    PlayerTwo = 999
+
+    while PlayerOne != RPS[0] and PlayerOne != RPS[1] and PlayerOne != RPS[2]:
+        PlayerOne = input("Player One, Rock, Paper, or Scissor?").capitalize()
+
+    while PlayerTwo != RPS[0] and PlayerTwo != RPS[1] and PlayerTwo != RPS[2]:
+        PlayerTwo = input("Player Two, Rock, Paper, or Scissor?").capitalize()
+
+    Winner = WinnerDecider(PlayerOne, PlayerTwo)
+
+    if Winner == "Draw":
+        print("There has been a draw!")
+        PlayAgain(GameMode)
+    else:
+        print(Winner, "is the big daddy grandmaster champion\n")
+        PlayAgain(GameMode)
+
+def PvCGame():
+    GameMode = "PvC"
+    RPS = ["Rock", "Paper", "Scissor"]
+    PlayerOne = 999
+    PlayerTwo = 999
+    while PlayerOne != RPS[0] and PlayerOne != RPS[1] and PlayerOne != RPS[2]:
+        PlayerOne = input("Player One, Rock, Paper, or Scissor?").capitalize()
+
+    PlayerTwo = RPS[int(random.uniform(1,3))]
+
+    Winner = WinnerDecider(PlayerOne, PlayerTwo)
+    print("The Computer chose", PlayerTwo, "\n")
+
+    if Winner == "Draw":
+        print("There has been a draw!")
+        PlayAgain(GameMode)
+    else:
+        print(Winner, "is the big daddy grandmaster champion\n")
+        PlayAgain(GameMode)
+
+def main():
+    Menu()
+
+main()
